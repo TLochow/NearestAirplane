7a421ce0346c376a70d6200c8032f15638fa3eba
Torben Lochow <t.lochow@gmx.de>
2018-09-02 12:00:05 +0200

    Grundversion.

---
+++ NearestAirplane/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using WebApiClient;
+
+namespace NearestAirplane {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Getting Airplanes...");
+            Task getAirplanesTask = new Task(GetAirplanes);
+            getAirplanesTask.Start();
+            Console.ReadLine();
+        }
+
+        public static async void GetAirplanes() {
+            string response = await WebClient.Get("https://opensky-network.org/api/states/all");
+            Console.Clear();
+            Console.Write(response);
+        }
+    }
+}
---
+++ WebApiClient/WebApiClient.cs
@@ -0,0 +1,12 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace WebApiClient {
+    public class WebClient {
+        public static async Task<string> Get(string url) {
+            HttpClient client = new HttpClient();
+            string response = await client.GetStringAsync(url);
+            return response;
+        }
+    }
+}
